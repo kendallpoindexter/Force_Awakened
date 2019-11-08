2890ca4642a706b79ce0446ac99b8162b5f1574a
Kendall <kendall.poindexter@detroitlabs.com>
2019-11-08 14:39:06 -0500

    Create FilmModel Struct

--- Project_Force_Awakened/Models/FilmModel.swift
+++ Project_Force_Awakened/Models/FilmModel.swift
@@ -7,3 +7,17 @@
 //
 
 import Foundation
+
+struct FilmModel {
+    let title: String
+    let episodeID: Int
+    let characterURLStrings: [String]
+}
+
+extension FilmModel {
+    enum CodingKeys: String, CodingKey {
+        case title
+        case episodeID = "episode_id"
+        case characterURLStrings = "characters"
+    }
+}
