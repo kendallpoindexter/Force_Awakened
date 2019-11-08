685e7124b7e026b60c51962c9b8ded14f6654a55
Kendall <kendall.poindexter@detroitlabs.com>
2019-11-08 14:00:09 -0500

    Create FilmModel file

---
+++ Project_Force_Awakened/Models/FilmModel.swift
@@ -0,0 +1,9 @@
+//
+//  FilmModel.swift
+//  Project_Force_Awakened
+//
+//  Created by Kendall Poindexter on 11/8/19.
+//  Copyright © 2019 Kendall Poindexter. All rights reserved.
+//
+
+import Foundation
