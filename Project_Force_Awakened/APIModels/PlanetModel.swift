f3900a1934c0382f30bff5eb58afe4b321e4e44f
Kendall <kendall.poindexter@detroitlabs.com>
2019-11-14 16:28:51 -0500

    Create Planet Model

---
+++ Project_Force_Awakened/APIModels/PlanetModel.swift
@@ -0,0 +1,13 @@
+//
+//  PlanetModel.swift
+//  Project_Force_Awakened
+//
+//  Created by Kendall Poindexter on 11/14/19.
+//  Copyright © 2019 Kendall Poindexter. All rights reserved.
+//
+
+import Foundation
+
+struct Planet: Decodable {
+    let name: String 
+}
